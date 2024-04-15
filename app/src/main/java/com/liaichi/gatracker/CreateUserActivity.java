7ed2388bd06c464db6388ad822bf189253d033a6
imiyasato <imiyasato@csumb.edu>
2024-04-15 00:23:05 -0700

    Set the bindings for landing, login, and create user activities, and misc.

--- app/src/main/java/com/liaichi/gatracker/CreateUserActivity.java
+++ app/src/main/java/com/liaichi/gatracker/CreateUserActivity.java
@@ -7,18 +7,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.liaichi.gatracker.database.GATRepository;
+import com.liaichi.gatracker.databinding.ActivityCreateUserBinding;
+import com.liaichi.gatracker.databinding.ActivityLoginPageBinding;
 
 public class CreateUserActivity extends AppCompatActivity {
+  private ActivityCreateUserBinding binding;
+
+  private GATRepository repository;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_create_user);
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-      return insets;
-    });
+    binding = ActivityCreateUserBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    repository = GATRepository.getRepository(getApplication());
+
   }
 }
--- app/src/main/java/com/liaichi/gatracker/LandingPageActivity.java
+++ app/src/main/java/com/liaichi/gatracker/LandingPageActivity.java
@@ -1,17 +1,28 @@
 package com.liaichi.gatracker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.liaichi.gatracker.database.GATRepository;
+import com.liaichi.gatracker.databinding.ActivityLandingPageBinding;
 
 public class LandingPageActivity extends AppCompatActivity {
-
+  private ActivityLandingPageBinding binding;
+  private GATRepository repository;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityLandingPageBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    repository = GATRepository.getRepository(getApplication());
 
+  }
+
+
+  static Intent landingPageActivityIntentFactory(Context context, int userID) {
+    Intent intent = new Intent(context, LandingPageActivity.class);
+    intent.putExtra(MAIN_ACTIVITY_USER_ID, userID);
+    return intent;
   }
 }
