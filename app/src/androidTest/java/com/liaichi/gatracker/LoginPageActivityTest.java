5a1d9c5e02bfb6072c614e2f8c583021c238bd0a
Lia Smith <70491947+liaberries@users.noreply.github.com>
2024-04-17 15:20:55 -0700

    Added test for LoginPageActivity

---
+++ app/src/androidTest/java/com/liaichi/gatracker/LoginPageActivityTest.java
@@ -0,0 +1,17 @@
+package com.liaichi.gatracker;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class LoginPageActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
